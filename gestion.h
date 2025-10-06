f87bfe1349207b3beaad519a04dc44185fd75e84
enzo21082005 <enzomartins78@hotmail.com>
2025-10-06 14:37:53 +0200

    Update gestion.h

--- gestion.h
+++ gestion.h
@@ -1,1 +1,43 @@
+#ifndef GESTION_H
+#define GESTION_H
+
+#define max_ligne 100
+#define max_colonne 200
+
+typedef struct voiture VEHICULE;
+struct voiture {
+    char direction;
+    int posx, posy;
+    char type;
+    char Carrosserie[4][30];
+    char etat;       // 1 = en mouvement, 0 = garée
+    int ticks_gare;  // nombre de ticks avant départ
+    char en_sortie;  // 0 = vers place, 1 = vers sortie
+    struct voiture *NXT;
+};
+
+// Structure pour représenter une place de parking
+typedef struct {
+    int x, y;
+    int libre; // 1 = libre, 0 = occupée
+} PLACE;
+
+// Coordonnées entrée/sortie
+#define ENTREE_X 1
+#define ENTREE_Y 18
+#define SORTIE_X 2
+#define SORTIE_Y 68
+
+// Fonctions voitures
+VEHICULE* creer_voiture(char type, int x, int y);
+void ajouter_voiture(VEHICULE** liste, VEHICULE* v);
+
+// Fonctions places
+void trouver_places(char plan[max_ligne][max_colonne]);
+PLACE* trouver_place_libre();
+
+// Déplacement
+void deplacer_voiture_vers(VEHICULE* v, PLACE* target);
+
+#endif
 
