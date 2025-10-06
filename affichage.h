97b8f2ebcf5ab531860c58f22451a3b2d3a9086c
enzo21082005 <enzomartins78@hotmail.com>
2025-10-06 14:37:12 +0200

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -1,1 +1,17 @@
+#ifndef AFFICHAGE.H
+#define AFFICHAGE.H
+#include "gestion.h"
+
+#define max_ligne 100
+#define max_colonne 200
+
+
+void charger_plan(const char *filename,char plan[max_ligne][max_colonne]);
+void afficher_plan(char plan[max_ligne][max_colonne],int ligne);
+void afficher_titre(const char *filename);
+void dessiner_voiture(VEHICULE* v, char plan[max_ligne][max_colonne]);
+void dessiner_voitures(VEHICULE* liste, char plan[max_ligne][max_colonne]);
+
+#endif
+
 
