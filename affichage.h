fd8582d1fc2719aa89ceeda1f2897f8b6823bfc1
enzo21082005 <enzomartins78@hotmail.com>
2025-12-01 09:17:32 +0100

    Update affichage.h for wide character support

--- affichage.h
+++ affichage.h
@@ -1,16 +1,17 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
+
+#include <ncursesw/ncurses.h>
 #include "gestion.h"
 
 #define max_ligne 100
 #define max_colonne 200
 
-
-void charger_plan(const char *filename,char plan[max_ligne][max_colonne]);
-void afficher_plan(char plan[max_ligne][max_colonne], VEHICULE* liste,int ligne);
+void charger_plan(const char *filename, wchar_t plan[max_ligne][max_colonne]);
+void afficher_plan(wchar_t plan[max_ligne][max_colonne], int lignes);
 void afficher_titre(const char *filename);
-void dessiner_voiture(VEHICULE* v, char plan[max_ligne][max_colonne]);
-void dessiner_voitures(VEHICULE* liste, char plan[max_ligne][max_colonne]);
+void dessiner_voiture(VEHICULE* v);
+void dessiner_voitures(VEHICULE* liste);
 
 #endif
 
